7f0869eba41ba1793e335bb362d5005a89389bff
merkol <merterkol@outlook.com>
2023-01-07 20:44:59 +0300

    Updates

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# import necessary OpenCV modules
+# Mert Erkol S017789 Department of Computer Science 
+
 import cv2
 import numpy as np
+from sklearn.cluster import MeanShift
+
 
 # read video file
-cap = cv2.VideoCapture("videos/vid-2-12.mp4")
+cap = cv2.VideoCapture("videos/vid-5-18.mp4")
 
 # sift object
 sift = cv2.SIFT_create()
@@ -15,11 +18,19 @@ bf = cv2.BFMatcher()
 old_frame = None
 new_frame = None
 
-right_counter = 0
-left_counter = 0
+# frame counter
+frame_count = 0
 
-dir_list = []
-count = 0
+# Fish counter
+fish_count = 0
+
+# Fish detection
+fish_detected = {0: '', 1: '', 2: ''}
+
+# Fish Exited or not
+fish_exited = {0: '', 1: '', 2: ''}
+
+
 while True:
     
     # read new frame from video
@@ -27,7 +38,7 @@ while True:
     
     if not success:
         break
-    
+ 
     new_frame = new_frame[0  : int(new_frame.shape[0]/3), :]
 
     if old_frame is None:
@@ -46,10 +57,11 @@ while True:
 
     # eliminate corners that are moving outside of the object
     for i in range(len(new_corners)):
-        if np.linalg.norm(new_corners[i] - old_corners[i]) < 20:
+        if np.linalg.norm(new_corners[i] - old_corners[i]) < 25:
             new_corners[i] = np.array([0, 0], dtype=np.float32)
             old_corners[i] = np.array([0, 0], dtype=np.float32)
-     
+    
+  
     # convert new corners to keypoints
     new_keypoints = [cv2.KeyPoint(x=c[0][0], y=c[0][1], size=10) for c in new_corners]
     
@@ -71,69 +83,125 @@ while True:
         if m.distance < 0.75 * n.distance:
             good_matches.append(m)
 
-    # Use the matches to find the transformation between the frames
-    if len(good_matches) > 3:
-        src_pts = np.float32([old_keypoints[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-        dst_pts = np.float32([new_keypoints[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-
-        M, mask = cv2.estimateAffine2D(src_pts, dst_pts, method = cv2.RANSAC)
-        
-        # Determine the object's moving direction
-        if M[0, 0] > 0 and M[1, 1] > 0:
-            print("The object is moving down and to the right")
-        elif M[0, 0] < 0 and M[1, 1] < 0:
-            print("The object is moving up and to the left")
-        elif M[0, 0] > 0 and M[1, 1] < 0:
-            print("The object is moving down and to the left")
-        elif M[0, 0] < 0 and M[1, 1] > 0:
-            print("The object is moving up and to the right")
-        else:
-            print("The object is not rotating")
-
-        # Determine the object's translation
-        translation_x = M[0, 2]
-        translation_y = M[1, 2]
-      
-        print(f"The object is moving by {translation_x} units along the x-axis and {translation_y} units along the y-axis")
-        
-        if (translation_y < 5 and translation_y > -5):
-            if translation_x > 0:
-                right_counter += 1
-            elif translation_x < 0:
-                left_counter += 1 
+    # After filtering the matches using the Lowe ratio, create a matrix of the keypoint coordinates optimum parameters (200, 25)
+    def get_cluster_points(keypoints, bandwidth=200, cluster_threshold=25):
+        points = np.zeros((len(good_matches), 2))
+        for i, match in enumerate(good_matches):
+            old_keypoint = keypoints[match.queryIdx]
+            points[i, :] = (old_keypoint.pt[0], old_keypoint.pt[1])
             
-        print(f"Right counter : {right_counter}")
-        print(f"Left counter : {left_counter}")
+        # Initialize the MeanShift algorithm with the points
+        clustering = MeanShift(bandwidth=bandwidth).fit(points)
+
+        # Extract the cluster labels for each point
+        labels = clustering.labels_
+        
+        # Draw the clusters on the image
+        for i, point in enumerate(points):
+            x, y = point
+            x = x.astype(int)
+            y = y.astype(int)
+            cluster = labels[i]
+            count_val = (labels == cluster).sum()
+            if count_val > cluster_threshold:
+                cv2.circle(new_frame, (x, y), 5, (0, 0, 255), -1)
+                cv2.putText(new_frame, str(cluster), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                
+        # Create a new list of points and labels for clusters with more than 25 points
+        filtered_points = [point for i, point in enumerate(points) if (labels == labels[i]).sum() > cluster_threshold]
+        filtered_labels = [label for i, label in enumerate(labels) if (labels == labels[i]).sum() > cluster_threshold]
+
+        # Update the points and labels arrays with the filtered lists
+        points = np.array(filtered_points)
+        labels = np.array(filtered_labels)
+
+        # Find the unique labels
+        unq = np.unique(labels).size
+        
+        # Find the cluster centers
+        centers = clustering.cluster_centers_
+        
+        # Deleting the deleted clusters center points 
+        if unq == 0:
+            centers = np.array([])
+        elif unq != centers.shape[0]:
+            sh = centers.shape[0] - unq
+            centers = np.delete(centers, np.s_[-sh:], axis=0)
+            
+        
+        for i,j in centers:
+            cv2.circle(new_frame, (int(i), int(j)), 5, (0, 0, 255), -1)
+            cv2.putText(new_frame, "Center", (int(i), int(j)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            
+    
+        return points, labels, centers
+    
+    datapoints = get_cluster_points(old_keypoints)    
+    
+    
+    # Use the matches to find the transformation between the frames
+    if len(good_matches) > 3 and datapoints[0].size != 0:
+        
+        for i in range(np.unique(datapoints[1]).size):
+            src_pts = np.float32([old_keypoints[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+            dst_pts = np.float32([new_keypoints[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+            
+            ## Filtering the points from the clusters
+            cluster_condition = np.where(datapoints[1] == i)[0]
+            clustered_points = datapoints[0][cluster_condition]
+            result = (src_pts[:, None] == clustered_points).all(-1).any(-1)
+            indexes = []
+            for it,j in enumerate(result):
+                if j == True:
+                    indexes.append(it)
+            index = np.isin(np.arange(dst_pts.shape[0]), indexes)
+            dst_pts = dst_pts[index]
 
         
-        # Apply the transformation to align the frames
-        h, w = old_gray.shape
-        aligned_frame = cv2.warpAffine(new_frame, M, (w, h)) 
         
-        # compute rotation matrix from affine transformation
-        M_rot = cv2.getRotationMatrix2D(center=(0, 0), angle = np.rad2deg(np.arctan2(M[1, 0], M[0, 0])), scale = 1)
+            # Estimate the affine transformation between the frames
+            M, mask = cv2.estimateAffine2D(clustered_points, dst_pts, method = cv2.RANSAC)
+        
+            # Determine the object's translation
+            translation_x = M[0, 2]
+            translation_y = M[1, 2]
+        
+            # Draw the object's trajectory
+            if i == 0:
+                color = (255, 255, 0)
+            elif i == 1:
+                color = (0, 255, 0)
+            
+            if fish_count < len(datapoints[2]) and fish_detected[i] != 'Detected':
+                st_point = (int(datapoints[2][fish_count][0]), int(datapoints[2][fish_count][1]))
+                fish_detected[fish_count] = 'Detected'
+                fish_count += 1
 
-        # compute angle of rotation from rotation matrix
-        angle = np.rad2deg(np.arctan2(M_rot[1, 0], M_rot[0, 0]))
-    
-        # determine direction of fish passage based on angle of rotation
-       
-        if angle > 0:
-            direction = "right"
-        elif angle < 0:
-            direction = "left"
-        else:
-            direction = "unknown"
+                
+            end_point = int(translation_x + st_point[0]), int(translation_y + st_point[1])
+            new_frame = cv2.line(new_frame, st_point, end_point, color,3)
+            st_point = end_point
+            if end_point[0] > 1364 and fish_exited[i] != 'Exited':
+                print(f"Fish {i} exited from frame from right at frame number: {frame_count} ", )
+                fish_exited[i] = 'Exited'
+            elif end_point[0] < 0 and fish_exited[i] != 'Exited':
+                print(f"Fish {i} exited from frame from left at frame number: {frame_count} ")
+                fish_exited[i] = 'Exited'
         
-        dir_list.append(direction)
-    
+    cv2.imshow("image", new_frame)
+    cv2.waitKey(0)
+        
+        #print(f"The object is moving by {translation_x} units along the x-axis and {translation_y} units along the y-axis")
+        
     # draw matches
     img_matches = cv2.drawMatches(old_frame, old_keypoints, new_frame, new_keypoints, good_matches, None,
                               flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
     # Show the matches
-    cv2.imshow('Matches', img_matches)
-    cv2.waitKey(0)  
+    #cv2.imshow('Matches', img_matches)
+    #cv2.waitKey(0)  
+    
+  
     
     # draw keypoints on new frame
     #new_frame_with_keypoints = cv2.drawKeypoints(new_frame, new_keypoints, None, color=(0, 255, 0))
@@ -146,4 +214,4 @@ while True:
 
     # update old frame and continue to next iteration
     old_frame = new_frame
-
+    frame_count+=1
